a72d0ceef89883ab162f36b41edae031464c2338
jgam <jgam@alumni.nd.edu>
2020-03-15 11:24:42 +0900

    useReducer

---
+++ src/Info.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+function Info() {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    console.log('name');
+    console.log({
+      name,
+      nickname
+    });
+  }, [name]);
+
+  const onChangeName = e => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = e => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>name:</b>
+          {name}
+        </div>
+        <div>
+          <b>nickname: </b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
